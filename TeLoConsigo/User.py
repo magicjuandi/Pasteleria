cf79ef1f291a599fd6d6d5d9d76396b1dee48b1f
magicjuandi <118768572+magicjuandi@users.noreply.github.com>
2024-05-10 22:08:12 -0500

    Delete TeLoConsigo directory

--- TeLoConsigo/User.py
+++
@@ -1,8 +0,0 @@
-import Type
-
-class User :
- def __init__(self, name = None, phone = None, email = None,type = Type):
-  self.name = name
-  self.phone = phone
-  self.email = email
-  self.type = type
